121f789fa6134a1826d3d6747339431e00faeafa
Logan Cole <emtlogancole@gmail.com>
2024-04-24 17:20:05 -0500

    chisel reads in and reverses the markdown

--- lib/chisel.rb
+++ lib/chisel.rb
@@ -8,13 +8,13 @@ class Chisel
   end
 end
 # Read the input file
-markdown = ARGV[0]
+markdown_file = ARGV[0]
 html_file = ARGV[1]
-markdown_file = File.read(markdown)
+markdown = File.read(markdown_file)
+html = Chisel.new(markdown).to_html
+File.write(html_file, html)
 # Convert it to MD
-Chisel.new(markdown_file).to_html
 # print out the return statement to the terminal`
-markdown_file_lines = markdown_file.lines.count
-html_file_lines = html_file.lines.count
-require 'pry'; binding.pry
+markdown_file_lines = markdown.lines.count
+html_file_lines = html.lines.count
 p "Converted #{markdown} (#{markdown_file_lines} lines) to #{html_file} (#{html_file_lines} lines)"
